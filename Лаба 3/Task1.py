bd8d6d62fa7ce6495a7d3f73926ea6e5550619c4
sviridov6012 <160075897+sviridov6012@users.noreply.github.com>
2024-11-03 18:32:32 +0300

    Лабораторная 3
    Свиридов Я.К

---
+++ Лаба 3/Task1.py
@@ -0,0 +1,14 @@
+def find_item_index(items_list, item):
+  for i, current_item in enumerate(items_list):
+    if current_item == item:
+      return i
+  return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_index(items_list, find_item)  # Вызываем функцию, чтобы получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
---
+++ Лаба 3/Task2.py
@@ -0,0 +1,19 @@
+def find_common_participants(participants_first_group, participants_second_group, delimiter=","):
+  first_group = participants_first_group.split(delimiter)
+  second_group = participants_second_group.split(delimiter)
+  common_participants = sorted(set(first_group) & set(second_group))
+  return common_participants
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+common_participants = find_common_participants(participants_first_group, participants_second_group, delimiter="|")
+
+print(f"Общие участники: {common_participants}")
+
+# Проверить работу функции с разделителем отличным от запятой
+participants_first_group = "Иванов,Петров,Сидоров"
+participants_second_group = "Петров,Сидоров,Смирнов"
+
+common_participants = find_common_participants(participants_first_group, participants_second_group)
+print(f"Общие участники: {common_participants}")
