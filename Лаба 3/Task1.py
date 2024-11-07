3bb10279656b08d2aacb69c1a2c7a49005632a9c
sviridov6012 <160075897+sviridov6012@users.noreply.github.com>
2024-11-07 19:15:13 +0300

    task1-2
    pep8

--- Лаба 3/Task1.py
+++ Лаба 3/Task1.py
@@ -1,8 +1,8 @@
 def find_item_index(items_list, item):
-  for i, current_item in enumerate(items_list):
-    if current_item == item:
-      return i
-  return None
+    for i, current_item in enumerate(items_list):
+        if current_item == item:
+            return i
+    return None
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
