9c7d858456b5870511571e2072e3d2996a75f937
sviridov6012 <160075897+sviridov6012@users.noreply.github.com>
2024-11-08 23:39:17 +0300

    Update Task2.py

--- Лаба 3/Task2.py
+++ Лаба 3/Task2.py
@@ -1,27 +1,19 @@
 def find_common_participants(participants_first_group, participants_second_group, delimiter=","):
-    first_group = participants_first_group.split(delimiter)
-    second_group = participants_second_group.split(delimiter)
-    common_participants = sorted(set(first_group) & set(second_group))
-    return common_participants
+  first_group = participants_first_group.split(delimiter)
+  second_group = participants_second_group.split(delimiter)
+  common_participants = sorted(set(first_group) & set(second_group))
+  return common_participants
 
-
-# Проверка работы функции с разделителем "|"
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-common_participants = find_common_participants(
-    participants_first_group,
-    participants_second_group,
-    delimiter="|"
-)
+common_participants = find_common_participants(participants_first_group, participants_second_group, delimiter="|")
+
 print(f"Общие участники: {common_participants}")
 
-# Проверка работы функции с разделителем ","
+
 participants_first_group = "Иванов,Петров,Сидоров"
 participants_second_group = "Петров,Сидоров,Смирнов"
 
-common_participants = find_common_participants(
-    participants_first_group,
-    participants_second_group
-)
+common_participants = find_common_participants(participants_first_group, participants_second_group)
 print(f"Общие участники: {common_participants}")
